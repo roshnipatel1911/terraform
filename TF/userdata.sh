8f7463c70697b7dc3efe95258c4d8e3b8cb02c66
Roshni Patel <roshni.aws1234@gmail.com>
2026-03-16 16:44:32 +0530

    Fix typo in nginx service start command

--- TF/userdata.sh
+++ TF/userdata.sh
@@ -4,4 +4,4 @@ sudo apt update
 sudo apt install nginx -y
 sudo systemctl start nignx
 sudo systemctl enable nginx
-echo "Hello World from AVYAAN" | sudo tee /var/www/html/index.html
+echo "Hello World" | sudo tee /var/www/html/index.html
